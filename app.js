64d2a5ab576ce12eac7f3324c5e4a6cf43b1da23
komisdaria <darijana701@gmail.com>
2021-04-27 16:07:00 +0300

    avtorization add

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
 const dbConnect = require('./db/dbConnect');
 const indexRouter = require('./routes/index');
+const registrationRouter = require('./routes/registration/registration');
 
 const mongoUrl = process.env.DATABASE_STRING;
 
@@ -22,6 +23,7 @@ app.use(express.json());
 
 // Add routers
 app.use('/', indexRouter);
+app.use('/registration', registrationRouter);
 
 const options = {
   store: MongoStore.create({ mongoUrl }),
@@ -36,7 +38,49 @@ const options = {
 
 const sessionMiddleware = session(options);
 
+// Добавляем юзера во все hbs
+app.use((req, res, next) => {
+  res.locals.username = req.session.username;
+  next();
+});
+
 app.use(sessionMiddleware);
+
+// Если HTTP-запрос дошёл до этой строчки, значит ни один из ранее встречаемых рутов не ответил на запрос.
+//  Это значит, что искомого раздела просто нет на сайте.
+//  Для таких ситуаций используется код ошибки 404.
+//  Создаём небольшое middleware, которое генерирует соответствующую ошибку.
+app.use((req, res, next) => {
+  const error = createError(404, 'Запрашиваемой страницы не существует на сервере.');
+  next(error);
+});
+
+// Отлавливаем HTTP-запрос с ошибкой и отправляем на него ответ.
+app.use((err, req, res, next) => {
+  // Получаем текущий ражим работы приложения.
+  const appMode = req.app.get('env');
+  // Создаём объект, в котором будет храниться ошибка.
+  let error;
+
+  // Если мы находимся в режиме разработки, то отправим в ответе настоящую ошибку.
+  //  В противно случае отправим пустой объект.
+  if (appMode === 'development') {
+    error = err;
+  } else {
+    error = {};
+  }
+
+  // Записываем информацию об ошибке и сам объект ошибки в специальные переменные,
+  //  доступные на сервере глобально, но только в рамках одного HTTP-запроса.
+  res.locals.message = err.message;
+  res.locals.error = error;
+
+  // Задаём в будущем ответе статус ошибки. Берём его из объекта ошибки, если он там есть. 
+  // В противно случае записываем универсальный стату ошибки на сервере - 500.
+  res.status(err.status || 500);
+  // Формируем HTML-текст из шаблона "error.hbs" и отправляем его на клиент в качестве ответа.
+  res.render('registration/error');
+});
 
 const PORT = 3000;
 

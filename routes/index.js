b465b5d939fe543eb8ea0d377be3071a6e0ab037
komisdaria <darijana701@gmail.com>
2021-04-28 14:10:47 +0300

    add link categories

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+const Category = require('../models/category.model');
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const categories = await Category.find();
+  res.locals.categories = categories;
   res.render('index');
 });
 
--- routes/linksinmenu/linksinmenu.js
+++ routes/linksinmenu/linksinmenu.js
@@ -1,11 +1,11 @@
-const { Router } = require('express');
-const Categoris = require('../../models/category.model');
+// const { Router } = require('express');
+// const Category = require('../../models/category.model');
 
-const router = Router();
+// const router = Router();
 
-router.get('/', async (req, res) => {
-  const categories = await Categoris.find();
-  res.render('find', { categories });
-});
+// router.get('/', async (req, res) => {
+//   const categories = await Category.find();
+//   res.render('find', { categories });
+// });
 
-module.exports = router;
+// module.exports = router;

e626ef5328949c17b8f044a19a0a8a3994b6d735
Michael <micklekoltsov@gmail.com>
2021-04-30 09:01:20 +0300

    Bug fixes, more styling

--- routes/categories/category.js
+++ routes/categories/category.js
@@ -1,15 +1,16 @@
-const Executor = require('../../models/executor.model');
 const { Router } = require('express');
+const Executor = require('../../models/executor.model');
+
 const router = Router();
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const executors = await Executor.find({categories: id});
+  const executors = await Executor.find({ categories: id });
   const filteredExecutors = executors.reduce((accum, executor) => {
     accum.push({
       id: executor.id,
-      username: executor.username
-    })
+      username: executor.username,
+    });
     return accum;
   }, []);
   res.render('categories/specificCategory', { filteredExecutors });

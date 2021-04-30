7b7ad90ae78f4ac5b72e3e2dc5cdc5572376f76a
Michael <micklekoltsov@gmail.com>
2021-04-30 13:40:09 +0300

    Fixes

--- routes/orders/order.js
+++ routes/orders/order.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const Category = require('../../models/category.model');
 const Order = require('../../models/order.model');
 const Customer = require('../../models/customer.model');
-const WorkType = require('../../models/workType.model')
+const WorkType = require('../../models/workType.model');
 
 const router = Router();
 
@@ -19,19 +19,33 @@ router.get('/fillyourorder', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const user = await Customer.findOne({ username: req.session.username });
-  // получаем заказ и создаем документ с этим заказом
-  const { title, description, deadline, categoryId, type } = req.body;
-  const typeId = await WorkType.findOne({ title: type });
-  await Order.create({
-    title,
-    category: categoryId,
-    type: typeId,
-    description,
-    customer: user.id,
-    deadline,
-  });
+  if (req.session.user_status === 'customer') {
+    const user = await Customer.findOne({ username: req.session.username });
+    // получаем заказ и создаем документ с этим заказом
+    const { title, description, deadline, category, type } = req.body;
+    const categoryId = await Category.findOne({ title: category });
+    const typeId = await WorkType.findOne({ title: type });
+    await Order.create({
+      title,
+      category: categoryId,
+      type: typeId,
+      description,
+      customer: user.id,
+      deadline,
+    });
+  }
   res.redirect('/');
 });
+
+// Посмотреть все заказы в категории
+router.get('/:id', async (req, res) => {
+  const orders = await Order.find({ category: req.params.id }).populate(
+    ['customer', 'category'],
+  );
+  res.render('orders/allOrders', { orders });
+});
+
+// Посмотреть конкретный заказ
+// router.get('/:id', async (req, res) => {});
 
 module.exports = router;

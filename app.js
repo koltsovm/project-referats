3a3f59d5909176ef3b620ed1156942a78b49f581
Michael <micklekoltsov@gmail.com>
2021-06-27 14:00:16 +0300

    Copied from local

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const mongoUrl = process.env.DATABASE_STRING;
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'hbs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan('dev'));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(morgan('dev'));
 
 const options = {
   store: MongoStore.create({ mongoUrl }),
